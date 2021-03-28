d7becd86d03762de8e911fd72fd2b7b64e6f2ebb
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-03-28 06:59:18 -0400

    Added models and the mongoose schema

---
+++ models/workout.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const WorkoutSchema = new Schema({
+
+  day: {
+    type: Date,
+    default: Date.now()
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Please enter an exercise type."
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Please enter an exercise name."
+      },
+      duration: {
+        type: Number,
+        required: false,
+      },
+      weight: {
+        type: Number,
+        required: false,
+      },
+      reps: {
+        type: Number,
+        required: false,
+      },
+      sets: {
+        type: Number,
+        required: false,
+      },
+      distance: {
+        type: Number,
+        required: false,
+      },
+    }
+  ],
+}, 
+{ toJSON: { virtual: true } }
+);
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
