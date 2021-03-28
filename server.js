debbb6a1206a4e564de3193b3b34411adfbcf339
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-03-28 06:54:16 -0400

    Added the public path to load the page

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"))
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
