f0ed0aa2d5a6214de8653f4a4be69e2b0ba7312e
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-03-30 07:10:23 -0400

    Added comments for htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,12 @@
+// Requiring path to use relative routes for HTML files
 const path = require("path");
 
-module.exports = function (app) {
+module.exports = app => {
+    // HTML redirect for Dashboard page
     app.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
-
+    // HTML redirect for Fitness Tracker page
     app.get("/exercise", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
